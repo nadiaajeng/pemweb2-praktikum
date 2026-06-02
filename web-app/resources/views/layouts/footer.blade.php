d188c192893f25df46dd82dc21399f07e9858a0d
nadiaajeng <nadiaajss12@gmail.com>
2026-06-02 19:29:11 +0700

    upload file praktikum

---
+++ web-app/resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="alert alert-primary text-center" role="alert">
+  <strong>Pemrograman Web 2</strong> <br>
+  <small>Semester Genap</small>
+</div>
+
+<footer class="text-center mt-4">
+  <hr>
+  <p class="mb-1">Dosen Pengampu: Pak Nasrul, S.Kom., M.Kom.</p>
+  <p class="mb-1">Nama: Nadia Ajeng Salsabila Saffa | NIM: 0110125068 | Prodi: Sistem Informasi</p>
+  <small class="text-muted">© 2026 STT Terpadu Nurul Fikri</small>
+</footer>
